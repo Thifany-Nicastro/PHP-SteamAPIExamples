09a2aa86d4ff61fafd8a2b4b32023ad0774d6268
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-11-14 15:46:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,29 +4,117 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Meta tags Obrigatórias -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="styles/main.css">
+    
+        <!-- Font Awesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" rel="stylesheet">
+    
+        <title>Inicio</title>
     </head>
     <body>
-        <h1><?=$json["response"]["players"][0]["personaname"];?></h1>
-        <img src="<?=$json["response"]["players"][0]["avatarfull"];?>">
-        <ul>
-            <li>SteamID64: <?=$json["response"]["players"][0]["steamid"];?></li>
-            <li>Display Name: <?=$json["response"]["players"][0]["personaname"];?></li>
-            <li>URL: <?=$json["response"]["players"][0]["profileurl"];?></li>
-            <li>Small Avatar: <img src='<?=$json["response"]["players"][0]["avatar"];?>'></li>
-            <li>Medium Avatar: <img src='<?=$json["response"]["players"][0]["avatarmedium"];?>'></li>
-            <li>Full Avatar: <img src='<?=$json["response"]["players"][0]["avatarfull"];?>'></li>
-            <li>Status: <?=personaState($json['response']['players'][0]['personastate']);?></li>
-            <li>Real Name: <?=$json["response"]["players"][0]["realname"];?></li>
-            <li>Joined: <?=$join_date;?></li>
-        </ul>
-        <ul>
-            <li> Jogo: <?=$json2[$appid]["data"]["name"];?></li>
-            <li> Desc: <?=$json2[$appid]["data"]["short_description"];?></li>
-            <li> Moeda: <?=$json2[$appid]["data"]["price_overview"]["currency"];?></li>
-            <li> Preço normal: <?=$json2[$appid]["data"]["price_overview"]["initial"];?></li>
-            <li> Preço atual: <?=$json2[$appid]["data"]["price_overview"]["final"];?></li>
-            <li> Preço atual F: <?=$json2[$appid]["data"]["price_overview"]["final_formatted"];?></li>
-            <li> Desconto: <?=$json2[$appid]["data"]["price_overview"]["discount_percent"];?></li>
-        </ul>
+        <main id="content" class="flex-grow-1">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-10">
+                        <div class="box-content">
+                            <h1><?=$json["response"]["players"][0]["personaname"];?></h1>
+                            <img src="<?=$json["response"]["players"][0]["avatarfull"];?>">
+                            <table class="table table-striped">
+                              <tbody>
+                                <tr>
+                                  <th scope="row">SteamID64</th>
+                                  <td><?=$json["response"]["players"][0]["steamid"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Display Name</th>
+                                  <td><?=$json["response"]["players"][0]["personaname"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">URL</th>
+                                  <td><a href='<?=$json["response"]["players"][0]["profileurl"];?>'>
+                                      <?=$json["response"]["players"][0]["profileurl"];?></a></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Avatar</th>
+                                  <td><img src='<?=$json["response"]["players"][0]["avatar"];?>'></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Status</th>
+                                  <td><?=personaState($json['response']['players'][0]['personastate']);?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Real Name</th>
+                                  <td><?=$json["response"]["players"][0]["realname"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Joined</th>
+                                  <td><?=$join_date;?></td>
+                                </tr>
+                              </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-10">
+                        <div class="box-content">
+                            <table class="table table-striped">
+                              <tbody>
+                                <tr>
+                                  <th scope="row">URL API</th>
+                                  <td><a href='<?=$api_url2;?>'><?=$api_url2;?></a></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">URL</th>
+                                  <td><a href='https://store.steampowered.com/app/<?=$appid;?>'>https://store.steampowered.com/app/<?=$appid;?></a></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">ID</th>
+                                  <td><?=$appid;?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Jogo</th>
+                                  <td><?=$json2[$appid]["data"]["name"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Descrição</th>
+                                  <td><?=$json2[$appid]["data"]["short_description"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Moeda</th>
+                                  <td><?=$json2[$appid]["data"]["price_overview"]["currency"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Preço original</th>
+                                  <td><?=$json2[$appid]["data"]["price_overview"]["initial"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Preço atual</th>
+                                  <td><?=$json2[$appid]["data"]["price_overview"]["final"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Preço atual formatado</th>
+                                  <td><?=$json2[$appid]["data"]["price_overview"]["final_formatted"];?></td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">Desconto atual</th>
+                                  <td><?=$json2[$appid]["data"]["price_overview"]["discount_percent"];?></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
